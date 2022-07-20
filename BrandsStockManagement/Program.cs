eb067eda3df3739f13e2224c629417483b14f5d9
Mudit Khanna <muditkhanna998@gmail.com>
2022-07-20 10:19:05 -0400

    Add project files.

---
+++ BrandsStockManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BrandsStockManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
